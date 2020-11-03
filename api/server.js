ea47f2f41d7cac6bbac4577a26282672e1294a4b
Nicolas Ruiz <nruiz@hexacta.com>
2020-11-03 00:37:20 -0300

    Fixed error handling middleware use

--- api/server.js
+++ api/server.js
@@ -3,18 +3,19 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 
-const unqfy = require("../api/middlewares/unqfy");
 const handleErrors = require("../api/middlewares/handleErrors");
+const unqfy = require("../api/middlewares/unqfy");
+
+app.use(bodyParser.json());
+app.use(unqfy);
 
 const artistRoutes = require('./routes/artistRoutes');
 const albumRoutes = require('./routes/albumRoutes');
 
-app.use(bodyParser.json());
-app.use(unqfy);
-app.use(handleErrors);
-
 app.use('/artists', artistRoutes);
 app.use('/albums', albumRoutes);
+
+app.use(handleErrors);
 
 app.listen(port, () => {
   console.log(`UNQfy app listening at http://localhost:${port}`);
