1d64ad98e3c6838af5dc6ce8d3f50ab0c4bb4436
Agustin Gonzalez <agustin-ag@live.com>
2020-12-06 13:40:39 -0300

    Se quito el link del webhook

--- monitor/reporter.js
+++ monitor/reporter.js
@@ -4,7 +4,7 @@ const reporter = {
   report : function (message) {
     /*
     const axiosInstance = axios.create({ baseURL: "https://discord.com/api"});
-    const endpoint = "/webhooks/783469014843916318/3M3qn2xCtHp0t-yL1NO2umdQ0WJPmo5tgDoQIOLMmbjTjna4YkVsCXL2RN_CQxTKr3s6";
+    const endpoint = "Endpoint";
     axiosInstance.post(endpoint, { content: message }); 
     */
     console.log(message);
